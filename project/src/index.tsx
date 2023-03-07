1ef7a2d87ada7529a1adefd5e406b6b90c3320b9
ChurkinaM <mapav4@yandex.ru>
2023-03-07 21:01:19 +0500

    правки по комментариям наставника

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -8,13 +8,11 @@ import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
     <ErrorBoundary>
       <Provider store={store}>
         <App />
       </Provider>
     </ErrorBoundary>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
